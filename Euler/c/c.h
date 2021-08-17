8b403303d87e8d19edb36087541609a58358ad42
vijay-rathinam <88542819+vijay-rathinam@users.noreply.github.com>
2021-08-17 13:11:24 +0530

    Euler 2nd program in c

--- Euler/c/c.h
+++ Euler/c/c.h
@@ -7,3 +7,19 @@ int sumOfMultiplesOf3and5(int num){
    }
    return sum;
 }
+
+long evenFibonacciSum(long num) {
+   long prev = 1, i = 0, sum = 0;
+   long current = prev;
+   long next = current + prev;
+   while(current < num) {
+       prev = current;
+       if(current % 2 == 0) {         
+          sum = sum + current;
+       }
+       current = next;
+       next = prev + current;
+       i++;
+   }
+   return sum;
+}
--- Euler/c/main.c
+++ Euler/c/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-#include "one.h"
+#include "c.h"
 
 int main() {
    int np1 ;
    printf("Enter number\n");
    scanf("%d", &np1);
    printf("The sum of multiples of 3 and 5 below number %d is %d\n", np1, sumOfMultiplesOf3and5(np1));
+
+   long np2;
+   printf("Enter fibonacci max number");
+   scanf("%ld", &np2);
+   printf("The sum of evenFibonacci below number %ld is %ld", np2, evenFibonacciSum(np2));
 }
